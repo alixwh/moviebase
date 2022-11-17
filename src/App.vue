2d40ad046d6a7775aff04b5abc3d5c50e339b05e
ali <hahinn@ttu.ee>
2022-11-18 00:11:01 +0200

    Move css to separate file

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,10 @@
           <li class="menu-list-item">
             Genres
           </li>
-          <router-link to="/about">
+          <router-link
+            to="/about"
+            class="menu-list-item"
+          >
             About
           </router-link>
         </ul>
@@ -38,120 +41,4 @@
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #151515;
-}
-
-.navbar {
-  width: 100%;
-  height: 80px;
-  background-color: black;
-}
-
-.navbar-container {
-  display: flex;
-  align-items: center;
-  padding: 0 25px;
-  height: 100%;
-  color: white;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 600;
-}
-
-.navbar a {
-  text-decoration: none;
-  color: inherit;
-}
-
-.logo-container {
-  color: #42b983;
-  font-size: 30px;
-  flex: 1;
-}
-
-.menu-container {
-  flex: 6;
-}
-
-.menu-list {
-  display: flex;
-  list-style: none;
-}
-
-.menu-list-item {
-  margin-right: 30px;
-}
-
-.profile-container {
-  flex: 2;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-}
-
-.profile-text {
-  margin: 0 20px;
-}
-
-.toggle {
-  width: 40px;
-  height: 20px;
-  background-color: white;
-  border-radius: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  position: relative;
-
-}
-
-.toggle-icons {
-  color: goldenrod;
-}
-
-.toggle-ball {
-  width: 18px;
-  height: 18px;
-  background-color: black;
-  position: absolute;
-  right: 1px;
-  border-radius: 50%;
-  cursor: pointer;
-  transition: 0.25s ease all;
-}
-
-#app.active {
-  background-color: white;
-  color: black;
-}
-
-.navbar-container.active {
-  background-color: #2b9f5f;
-  color: black;
-}
-
-.logo-container.active {
-  color: black;
-}
-
-.toggle.active {
-  background-color: black;
-}
-
-.toggle-ball.active {
-  background-color: white;
-  transform: translateX(-20px);
-}
-
-@media only screen and (max-width: 940px) {
-  .menu-container {
-    display: none;
-  }
-}
-</style>
+<style src="./App.css" scoped />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="discover-container">
       <div class="search-container">
         <form
-          action="http://localhost:8080/api/search"
+          action="/search"
           method="get"
           class="search-bar"
         >
@@ -22,7 +22,7 @@
       </div>
       <div class="quiz-container">
         <h1>Don't know what to watch?</h1>
-        <h2>Anwer questions and we will find you something you'll like.</h2>
+        <h2>Answer questions and we will find you something you'll like.</h2>
       </div>
     </div>
     <div class="content-container">
@@ -71,114 +71,4 @@ const getMovies = async () => {
 onMounted(() => getMovies());
 </script>
 
-<style scoped>
-ul {
-  list-style: none;
-  display: flex;
-  padding: 0 20px;
-  height: 400px;
-  transform: translateX(0);
-  transition: 1s ease-in-out;
-}
-
-li {
-  margin-right: 30px;
-  position: relative;
-}
-
-.home {
-  color: white;
-}
-
-.discover-container{
-  background-color: black;
-  margin: 0 10%;
-  border-radius: 0 0 50px 50px;
-}
-.search-container {
-  min-height: 15vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.search-bar {
-  width: 100%;
-  max-width: 600px;
-  max-height: 30px;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  border-radius: 50px;
-  padding: 10px 20px;
-}
-
-.search-bar input {
-  flex: 1;
-  border: 0;
-  outline: none;
-  font-size: 20px;
-  padding: 2px 10px;
-  background-color: transparent;
-}
-
-.search-bar button {
-  border: 0;
-  max-height: 30px;
-  border-radius: 50%;
-  width: 60px;
-  background-color: transparent;
-  cursor: pointer;
-}
-
-.magnifying-glass {
-  font-size: 20px;
-  color: darkgray;
-}
-
-.quiz-container {
-  height: 40vh;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 50px;
-}
-
-.content-container {
-  height: 70vh;
-}
-
-.movie-list-title {
-  display:flex;
-  margin-left: 30px;
-  margin-bottom: 0;
-}
-
-.movie-list-wrapper {
-  position: relative;
-  overflow: hidden;
-}
-
-.arrow {
-  font-size: 120px;
-  position: absolute;
-  top: 70px;
-  right: 2%;
-  color: lightgray;
-  opacity: 0.5;
-  cursor: pointer;
-}
-
-.home.active {
-  color: black;
-}
-
-.search-bar.active {
-  background-color: gray;
-}
-
-.discover-container.active{
-  background-color: #42b977;
-}
-
-</style>
+<style src="./HomeView.css" scoped />
